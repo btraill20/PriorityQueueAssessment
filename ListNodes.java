bb5bd4704763ad8fe44f39e9ae7e1719dc51bf47
btraill20 <35263705+btraill20@users.noreply.github.com>
2018-06-24 18:37:00 +0100

    Add files via upload

---
+++ ListNodes.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+/**
+ *
+ * @author Ben
+ */
+public class ListNodes<T>
+{
+    //this class is for setting up nodes, and their next node.
+    private T item;
+    private ListNodes<T> next;
+    
+    public ListNodes(T item, ListNodes<T> next)
+    {
+        this.item = item;
+        this.next = next;
+    }
+    
+    public T getItem()
+    {
+        return item;
+    }
+    
+    public ListNodes<T> getNext()
+    {
+        return next;
+    }
+}
