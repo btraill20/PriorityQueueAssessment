2a374d9ad484799793775dccee8b19fff3d41fd3
btraill20 <35263705+btraill20@users.noreply.github.com>
2018-06-25 12:26:13 +0100

    tests

---
+++ src/queuemanager/UnsortedArray.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+/**
+ *
+ * @author Ben
+ */
+public class UnsortedArray<T> implements PriorityQueue<T> {
+    //setting the varibles for the class, these are private as not to conflict with the other classes.
+    private final Object[] storage;
+    private final int capacity;
+    private int tailIndex;
+    
+    //constructor used to initiate all used varibles.
+    //the varibles used are storage, capacity and tail index.
+    public UnsortedArray(int size)
+    {
+        storage = new Object[size];
+        capacity = size;
+        tailIndex = -1;
+    }
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        tailIndex = tailIndex + 1;
+        if (tailIndex >= capacity) {
+            /* No resizing implemented, but that would be a good enhancement. */
+            tailIndex = tailIndex - 1;
+            throw new QueueOverflowException();
+        } else {
+            /* Scan backwards looking for insertion point */
+            int i = tailIndex;
+            storage[i] = new PriorityItem<>(item, priority);
+        }
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            return ((PriorityItem<T>) storage[0]).getItem();
+        }
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            for (int i = 0; i < tailIndex; i++) {
+                storage[i] = storage[i + 1];
+            }
+            tailIndex = tailIndex - 1;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tailIndex < 0;
+    }
+    
+        @Override
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i <= tailIndex; i++) {
+            if (i > 0) {
+                result = result + ", ";
+            }
+            result = result + storage[i];
+        }
+        result = result + "]";
+        return result;
+    }
+    
+}
