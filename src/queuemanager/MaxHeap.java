6b0cec44b47124a012e62582413e55b9fde46a3c
btraill20 <35263705+btraill20@users.noreply.github.com>
2018-06-29 18:53:50 +0100

    heap initial commit

---
+++ src/queuemanager/MaxHeap.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+/**
+ *
+ * @author Ben
+ */
+public class MaxHeap  <T>  implements PriorityQueue<T> {
+    
+    private ListNodes<T> top;
+
+    private int size()
+    {
+        ListNodes<T> node = top;
+        int result = 0;
+        while(node != null)
+        {
+            result = result + 1;
+            node = node.getNext();
+        }
+        return result;
+    }
+    
+    public MaxHeap(int size)
+    {
+        top = null;
+
+    }
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        top = new ListNodes<>(item,top);
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            return top.getItem();
+        }
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } 
+        top = top.getNext();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+    
+    @Override
+    public String toString() {
+        String result = "Sorted Linked list size =" + size();
+        result += ", contents = [";
+                for(ListNodes<T> node = top; node != null; node = node.getNext())
+                {
+                    if(node != top)
+                    {
+                    result += ", ";
+                    }
+                    result += node.getItem();
+                }
+        return result;
+    }
+    
+    
+}
